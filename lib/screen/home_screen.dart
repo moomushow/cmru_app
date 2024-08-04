5d20d985277019a52b053cafc7d8305f083dd67b
moomushow <teerapong_jai@hotmail.com>
2024-08-04 21:27:32 +0700

    ShowAbout

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -34,15 +34,82 @@ class _HomeScreenState extends State<HomeScreen> {
             'Home',
           ),
           backgroundColor: Colors.blue.shade500),
-      body: Center(
-        child: Text('สวัสดีคุณ $name'),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Image.asset('assets/landscape.jpg'),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Text("การอบรมหลักสูตร Flutter"),
+                    Text(
+                      "โดยนายบัณฑิต นันทะเทศ",
+                      style: TextStyle(
+                        color: Colors.red.shade500,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.favorite),
+                    Text("41"),
+                  ],
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Icon(Icons.call),
+                    Text(
+                      "Call",
+                      style: TextStyle(
+                        color: Colors.red.shade500,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.message),
+                    Text("Message"),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.email),
+                    Text("Email"),
+                  ],
+                ),
+              ],
+            ),
+            Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: changeName,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
-      // This trailingmma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Colors.blue.shade500,
+        showUnselectedLabels: true,
+        unselectedItemColor: Colors.blue.shade500,
+        onTap: (int index) {},
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
+          BottomNavigationBarItem(icon: Icon(Icons.call), label: 'Item'),
+          BottomNavigationBarItem(icon: Icon(Icons.info), label: 'About'),
+        ],
+      ),
     );
   }
 }
